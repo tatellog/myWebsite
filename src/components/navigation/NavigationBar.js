4d5f535cc112cb7d310caae25eab55350ec5897c
taniaTello <taniatello@MacBook-Pro-de-taniaTello.local>
2018-11-08 23:07:06 -0600

    Config routes

---
+++ src/components/navigation/NavigationBar.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Route, Link} from 'react-router-dom';
+
+import HomePage from "./../../pages/HomePage";
+import AboutPage from "./../../pages/AboutPage";
+import SkillsPage from './../../pages/SkillsPage';
+import ContactPage from './../../pages/ContactPage';
+
+
+const NavigationBar = () => {
+	const routes =[
+		{
+		  path: '/',
+		  exact :true,
+		  sidebar : () => <HomePage/>,
+		},
+		{
+		  path: '/about',
+		  exact :true,
+		  sidebar : () => <AboutPage/>
+		},
+		{
+		  path: '/skills',
+		  exact :true,
+		  sidebar : () => <SkillsPage/>,
+		},
+		{
+		  path: '/contact',
+		  exact :true,
+		  sidebar : () => <ContactPage/>,
+		},
+	  
+	  ]
+	  
+	return (
+		<container>
+			<div className='menu-nav__header'>
+				<ul>
+				<li><Link to = '/'>Home /</Link></li>
+				<li><Link to = '/about'>About /</Link></li>
+				<li><Link to = '/skills'>Skills /</Link></li>
+				<li><Link to = '/contact'>Contact /</Link></li>
+				</ul>
+
+				{routes.map((route) => (
+				<Route
+					key={route.path}
+					path={route.path}
+					exact={route.exact}
+					component={route.sidebar}
+				/>
+				))}
+			</div>
+		</container>
+	)
+}
+export default NavigationBar
